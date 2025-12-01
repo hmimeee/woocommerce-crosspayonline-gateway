a7f0211924d5a5d41d6238bb8925b24adbd26326
hmimeee <hmimeee@gmail.com>
2025-12-01 20:38:53 +0600

    Fixed spacemit

--- classes/WC_CrossPayOnline_Spaceremit_Gateway.php
+++ classes/WC_CrossPayOnline_Spaceremit_Gateway.php
@@ -2,12 +2,12 @@
 
 class WC_CrossPayOnline_Spaceremit_Gateway extends WC_CrossPayOnline_Gateway
 {
-    public string $key = 'spaceremit';
+    public string $key = 'spacemit';
 
     public function __construct()
     {
         parent::__construct();
-        $this->method_title       = __('CrossPayOnline Spaceremit', $this->id);
-        $this->method_description = __('Pay with Spaceremit via CrossPayOnline.', $this->id);
+        $this->method_title       = __('CrossPayOnline Spacemit', $this->id);
+        $this->method_description = __('Pay with Spacemit via CrossPayOnline.', $this->id);
     }
 }
